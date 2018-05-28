11884429584c03eeb292ee6dccf52b8c405ba89c
Daniele Esposti <daniele.esposti@gmail.com>
2018-05-28 22:55:01 +0100

    Added pre-commit hook for code formatting

---
+++ Makefile
@@ -0,0 +1,5 @@
+init:
+	git config core.hooksPath .githooks
+
+format:
+	black -l99 .
--- transformations/remove_annotations_transformer.py
+++ transformations/remove_annotations_transformer.py
@@ -46,14 +46,13 @@ class RemoveAnnotationsTransformer(astor.tree_walk.TreeWalk):
         Remove import of `typing` package
         """
         for name in self.cur_node.names:
-            if name.name == 'typing':
+            if name.name == "typing":
                 self.replace(None)
                 break
-
 
     def pre_ImportFrom(self):
         """
         Remove import from `typing` package
         """
-        if self.cur_node.module == 'typing':
+        if self.cur_node.module == "typing":
             self.replace(None)
